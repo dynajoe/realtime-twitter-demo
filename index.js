5cdea94db9ce78c7081d96cd90532f4fee949c2f
Joe Andaverde <joe.andaverde@gmail.com>
2015-12-03 00:11:51 -0600

    Added exclude filter - update for demo to kiddos

--- index.js
+++ index.js
@@ -2,30 +2,47 @@ var io = require('socket.io'),
     express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = io.listen(server),
+    io = io.listen(server, { log: false }),
     path = require('path'),
-    twitter = require('ntwitter'),
-    excluded = require('./excluded.json');
+    twitter = require('twit'),
+    excluded = require('./data/excluded.json'),
+    _ = require('lodash');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-var twit = new twitter(require('./twitter-config.json'));
-   
-var term = "sharkweek";
+var T = new twitter(require('./twitter-config.json'));
 
-twit.stream('statuses/filter',{ track: term}, function (stream) {
-   stream.on('data', function (data) {
-      for (var i = 0; i < excluded.length; i++) 
-      {
-         if (data.text.toLowerCase().indexOf(excluded[i]) > -1) 
-            return;
-      }
+var currentStream = null;
 
-      io.sockets.emit('data', {term: term, data: data});
-   });  
+io.on('connection', function (socket) {
+  socket.on('term', function (newTerm) {
+    startStream(newTerm);
+  });
 });
+
+var startStream = function (term) {
+  if (currentStream) {
+    currentStream.stop();
+  }
+
+  currentStream = T.stream('statuses/filter', { track: term });
+
+  currentStream.on('tweet', function (data) {
+    var loweredText = data.text.toLowerCase();
+
+    var containsFilteredWord = _.any(excluded, function (w) {
+      return loweredText.indexOf(w) > -1;
+    });
+
+    if (containsFilteredWord) {
+      return;
+    }
+
+    io.sockets.emit('data', { term: term, data: data });
+  });
+};
 
 app.get('/', function (req, res) {
    res.render('index');
